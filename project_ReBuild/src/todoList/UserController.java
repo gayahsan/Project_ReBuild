2c96c00e7d2d7a347b8ad6c9535a533e9eb8adc8
raviK123456 <ravindukaushan1@gmail.com>
2021-07-01 00:22:07 +0530

    Create UserController.java

---
+++ project_ReBuild/src/todoList/UserController.java
@@ -0,0 +1,62 @@
+package todoList;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/UserController")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDao userDao;
+	
+	public void init() {
+		userDao = new UserDao();
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.sendRedirect("register/register.jsp");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+	
+	private void register(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUserName(username);
+        user.setPassword(password);
+
+        try {
+            int result = userDao.registerUser(user);
+            if (result == 1) {
+                request.setAttribute("NOTIFICATION", "User Registered Successfully!");
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("register/register.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
+}
