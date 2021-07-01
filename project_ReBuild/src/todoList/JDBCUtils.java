a41202df1b487d02aa46266a07e2f83928d1d5e2
raviK123456 <ravindukaushan1@gmail.com>
2021-07-02 00:54:42 +0530

    Update JDBCUtils.java

--- project_ReBuild/src/todoList/JDBCUtils.java
+++ project_ReBuild/src/todoList/JDBCUtils.java
@@ -1,7 +1,10 @@
 package todoList;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class JDBCUtils {
 	
@@ -28,4 +31,27 @@ public class JDBCUtils {
 		
 	}
 
+	public static void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+
+	public static Date getSQLDate(LocalDate date) {
+		return java.sql.Date.valueOf(date);
+	}
+
+	public static LocalDate getUtilDate(Date sqlDate) {
+		return sqlDate.toLocalDate();
+	}
 }
